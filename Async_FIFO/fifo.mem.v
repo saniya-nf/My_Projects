0320dfaffb56a1e8d4edf9c1af1f8f5e1ad52fd1
saniya-nf <89337618+saniya-nf@users.noreply.github.com>
2026-06-08 16:44:34 +0530

    parameterized FIFO mem array

---
+++ Async_FIFO/fifo.mem.v
@@ -0,0 +1,20 @@
+module fifo_mem #(parameter DATA_WIDTH=8,ADDR_WIDTH=3)(
+output [DATA_WIDTH-1:0]rdata,
+input wclk,we,
+input [DATA_WIDTH-1:0]wdata,
+input [ADDR_WIDTH-1:0]waddr,raddr
+);
+
+reg [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
+
+//write operation
+always@(posedge wclk)
+  if(we)
+    mem[waddr] <= wdata;
+//else mem[waddr] <= mem[waddr]; //no need to write coz mem automatically holds value .  the same data again, it will be retained in the memory
+  
+  //read operation - async coz if we use clk here then fifo wont be fast anymore it will depend on the rising edge of clk.
+  assign rdata = mem[raddr];
+endmodule
+
+
