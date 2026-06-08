d640d65e03c9ccc674c8e56d17347dbb62fc98d7
saniya-nf <89337618+saniya-nf@users.noreply.github.com>
2026-06-08 16:47:55 +0530

    parameterized FIFO mem array

--- Async_FIFO/fifo.mem.v
+++ Async_FIFO/fifo.mem.v
@@ -9,7 +9,9 @@ reg [DATA_WIDTH-1:0] mem [0:(1<<ADDR_WIDTH)-1];
 
 //write operation
 always@(posedge wclk)
-  if(we)
+   /*if(rst) ,this  is not needed here coz fifo rst pointers already do that , pointers reset when both 
+      show same locations. FIFO appears empty --old data never read, so no need to clr mem with rst*/
+    if(we)
     mem[waddr] <= wdata;
 //else mem[waddr] <= mem[waddr]; //no need to write coz mem automatically holds value .  the same data again, it will be retained in the memory
   
