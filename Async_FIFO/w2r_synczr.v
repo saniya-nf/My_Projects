80db1588793f2bd59b5725f2133ada48d0e4b04a
saniya-nf <89337618+saniya-nf@users.noreply.github.com>
2026-08-11 16:42:32 +0530

    w2r synchronizer logic

--- Async_FIFO/w2r_synczr.v
+++ Async_FIFO/w2r_synczr.v
@@ -1,13 +1,17 @@
 module w2r_synczr (
-    output wptr_rsync,
-    input wptr,
-    input rst,wclk,rclk
+    output reg [6:0]wq2_rptr,//output from FF2
+    input [6:0]wptr_gray,
+    input rrst_n,rclk
 );
+    reg[6:0]wq1_rptr;//FF1
 
-reg [ADDR_WIDTH:0] sync1,sync2;
-
-    //ff
-    always@(posedge wclk or negedge rst)
-        if(!rst)
-            
+    always@(posedge rclk ,negedge rrst_n)
+        if(!rrst_n)begin
+            wq1_rptr<=7'b0;
+            wq2_rptr<=7'b0;
+        end
+        else begin
+            wq1_rptr<=wptr_gray;//FF1 catches the wptr_gray from write clock domain
+            wq2_rptr<=wq1_rptr;//FF2 stabilizes the wptr_gray to be used in read clock domain
+        end
 endmodule
