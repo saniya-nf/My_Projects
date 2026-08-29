cd68b5368015300647d7110cd2aff0ac3f632363
saniya-nf <89337618+saniya-nf@users.noreply.github.com>
2026-08-30 00:40:10 +0530

    Add RTL modules: AXI master, slave, FIFO, bridge FSM, APB slave, top

---
+++ AXI4Lite_to_DualAPB_Bridge/rtl/AXI_master.v
@@ -0,0 +1,64 @@
+module axi_master(
+    //write channel
+    input clk,rst,
+/*#1*/output reg [31:0]AWADDR,
+    output reg AWVALID,
+    input AWREADY,
+    
+/*#2*/output reg [31:0]WDATA,
+    output reg WVALID,
+    output reg[3:0]WSTRB,//we check for which byte is valid of data therefr in data channel
+    input WREADY,
+
+/*#3*/input [1:0]BRESP,
+    input BVALID,
+    output reg BREADY,
+
+    //read channel
+/*#4*/output reg [31:0]ARADDR,
+    output reg ARVALID,
+    input ARREADY, 
+    
+/*#5*/input [31:0]RDATA,
+input RVALID,
+output reg RREADY,
+input [1:0] RRESP
+);
+
+always@(posedge clk,posedge rst)
+    if(rst)begin
+        //write channel
+        AWADDR<=0;//ALL output reg signals → reset to 0!
+        AWVALID<=0;
+        WDATA<=0;
+        WVALID<=0;
+        BREADY<=0;
+        WSTRB<=0;
+
+        //read channel
+        ARADDR<=0;
+        ARVALID<=0;
+        RREADY<=0;
+    end
+    else begin
+        //transactions -Step 1 — put address and data on bus
+        AWADDR<=32'h0010_0010;//i gave random address to store my data into
+        AWVALID<=1;
+        WDATA<=32'hBCCC_CCCC;//data
+        WVALID<=1;
+        WSTRB <= 4'hF;//all bytes are valid for a simple design
+        BREADY<=1;
+
+        // Step 2 — wait for handshake
+        if(AWREADY && WREADY)begin
+            AWVALID<=0;//this shows handsake is done
+            WVALID<=0;
+        end
+
+        //step3-response received
+        if(BVALID)
+            BREADY<=0;//as BREADY is always 1 , after slave asserts bready=1, which means handshake is complete so ,master deasserts bready for the next write to happen
+
+
+    end
+endmodule
---
+++ AXI4Lite_to_DualAPB_Bridge/rtl/AXI_slave_IF.v
@@ -0,0 +1,81 @@
+module axi_slave(
+    input clk,rst,
+    //write addr channel
+    input [31:0] AWADDR,
+    input AWVALID,
+    output reg AWREADY,
+    
+
+    //write DATA channel
+    input [31:0] WDATA,
+    input WVALID,
+    input [3:0]WSTRB,
+    output reg WREADY,
+    
+    output reg [1:0]BRESP,
+    output reg BVALID,
+    input BREADY,
+
+    input [31:0]ARADDR,
+    input ARVALID,
+    output reg ARREADY,
+
+    output reg [31:0]RDATA,
+    output reg RVALID,
+    output reg[1:0] RRESP,
+    input RREADY,
+
+    //TO FIFO
+    output fifo_wen, //wire coz assign
+    output [64:0]fifo_wdata,//wire
+    input fifo_full
+);
+
+    wire fifo_wen;
+    wire [64:0]fifo_wdata;
+
+    always@(posedge clk,posedge rst)
+    if(rst)begin
+        //write channel
+        AWREADY<=0;//ALL output reg signals → reset to 0!
+        WREADY<=0;
+        BRESP<=0;
+        BVALID<=0;
+        /*fifo_wen<=0;
+        fifo_wdata<=0; BUG-fifo_wen and fifo_wdata are wires — can't reset in always block:*/
+
+        //read channel
+        ARREADY<=0;
+        RDATA<=0;
+        RVALID<=0;
+        RRESP<=0;
+    end
+    else begin
+        if( AWVALID && !fifo_full)
+            AWREADY<=1;/*// If condition false → AWREADY HOLDS old value!
+                        // = stays 1 forever after first handshake!  */
+        else 
+            AWREADY<=0;//with else it deasserts when not needed
+        
+        if(WVALID &&!fifo_full)
+            WREADY<=1;
+        else
+            WREADY <= 0;  // deassert!
+        
+
+        if(fifo_wen)begin 
+            BRESP<=2'b00;
+            BVALID<=1;
+        end
+        else if(BVALID && BREADY)
+            BVALID<=0;//handshaking is done
+
+        if(ARVALID && !fifo_full)
+            ARREADY<=1;
+        else
+            ARREADY <= 0;
+
+    end
+
+assign fifo_wen = (AWVALID & AWREADY & WVALID  & WREADY); 
+assign fifo_wdata = {1'b1, AWADDR, WDATA}; 
